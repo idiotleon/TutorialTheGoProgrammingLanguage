334f115df194319f47f83ac7eb18c8a2f4200851
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-01-16 20:08:20 -0800

    demo example: user structed type/class
    Module 3.11 Basic User Types

--- example_class_user_defined.go
+++ example_class_user_defined.go
@@ -4,9 +4,18 @@ import "fmt"
 
 type SalutationSimple string
 
+type SalutationStructed struct {
+	name     string
+	greeting string
+}
+
 func main() {
 
-	var message SalutationSimple = "Hello Go World from user defined type [simple]"
+	var message SalutationSimple = "Hello Go World from user defined type! [simple]"
 
 	fmt.Print(message)
+
+	var messageStructed = SalutationStructed{"leon", "Hello Go World from user defined type! [structed]"}
+
+	fmt.Println(messageStructed.name, messageStructed.greeting)
 }
