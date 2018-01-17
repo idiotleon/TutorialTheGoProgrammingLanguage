be07ffe951b74e3a173f8846749fe84920fe8a84
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-01-16 20:20:48 -0800

    demo example: another way of assignment to the structed type
    Module 3.11 Basic User Types

--- example_class_user_defined.go
+++ example_class_user_defined.go
@@ -4,6 +4,14 @@ import "fmt"
 
 type SalutationSimple string
 
+/*
+*	With a Captical S, it means that this is publicly visible
+*	outside of this package
+*	So if we were to put this in its own package and import that package,
+*	anything that has a capital is going to be visible and anything,
+*	which is NOT the case in terms of lower case initials.
+*	So there is no need for private and protected in public
+ */
 type SalutationStructed struct {
 	name     string
 	greeting string
@@ -13,9 +21,16 @@ func main() {
 
 	var message SalutationSimple = "Hello Go World from user defined type! [simple]"
 
-	fmt.Print(message)
+	fmt.Println(message)
 
 	var messageStructed = SalutationStructed{"leon", "Hello Go World from user defined type! [structed]"}
 
 	fmt.Println(messageStructed.name, messageStructed.greeting)
+
+	// another way to assign values
+	var messageStructed2 = SalutationStructed{}
+	messageStructed2.name = "Leon"
+	messageStructed2.greeting = "Hello Go World from user structed type 2! [structed 2]"
+
+	fmt.Println(messageStructed2.name, messageStructed2.greeting)
 }
